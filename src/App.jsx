cc823d72bb1aa5962c3edb4f313515cdf4e5af06
Mesut Kutlu <mkutlu13@gmail.com>
2022-03-08 11:05:50 -0600

    Login, Register and Dashboard Components introduced.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,131 @@
-import Login from './components/Login'
-
-function App() {
-  return (
-    <div>
-      <Login/>
-    </div>
-  );
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import Login from "./components/login.component";
+import Register from "./components/register.component";
+import Home from "./components/dashboard.component";
+import Profile from "./components/profile.component";
+import DashboardStudent from "./components/dashboard-student.component";
+import DashboardFaculty from "./components/dashboard-faculty.component";
+import DashboardAdmin from "./components/dashboard-admin.component";
+import { logout } from "./actions/auth";
+import { clearMessage } from "./actions/message";
+import { history } from './helpers/history';
+import Navbar from "./components/navbar.component";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.logOut = this.logOut.bind(this);
+    this.state = {
+      showModeratorBoard: false,
+      showAdminBoard: false,
+      currentUser: undefined,
+    };
+    history.listen((location) => {
+      props.dispatch(clearMessage()); // clear message when changing location
+    });
+  }
+  componentDidMount() {
+    const user = this.props.user;
+    if (user) {
+      this.setState({
+        currentUser: user,
+        showModeratorBoard: user.roles.includes("ROLE_MODERATOR"),
+        showAdminBoard: user.roles.includes("ROLE_ADMIN"),
+      });
+    }
+  }
+  logOut() {
+    this.props.dispatch(logout());
+  }
+  render() {
+    const { currentUser, showModeratorBoard, showAdminBoard } = this.state;
+    return (
+      <Router>
+        <Navbar/>
+        <div>
+        {/*<nav className="navbar navbar-expand navbar-dark bg-dark">
+            <Link to="/" className="navbar-brand">
+              bezKoder
+            </Link>
+            <div className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link to="home" className="nav-link">
+                  Home
+                </Link>
+              </li>
+              {showModeratorBoard && (
+                <li className="nav-item">
+                  <Link to="mod" className="nav-link">
+                    Moderator Board
+                  </Link>
+                </li>
+              )}
+              {showAdminBoard && (
+                <li className="nav-item">
+                  <Link to="admin" className="nav-link">
+                    Admin Board
+                  </Link>
+                </li>
+              )}
+              {currentUser && (
+                <li className="nav-item">
+                  <Link to="user" className="nav-link">
+                    User
+                  </Link>
+                </li>
+              )}
+            </div>
+            {currentUser ? (
+              <div className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link to="profile" className="nav-link">
+                    {currentUser.username}
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <a href="/login" className="nav-link" onClick={this.logOut}>
+                    LogOut
+                  </a>
+                </li>
+              </div>
+            ) : (
+              <div className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link to="login" className="nav-link">
+                    Login
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="register" className="nav-link">
+                    Sign Up
+                  </Link>
+                </li>
+              </div>
+            )}
+          </nav>*/}
+          <div className="container mt-3">
+            <Routes>
+              <Route exact path="/" element={<Home/>} />
+              <Route exact path="/login" element={<Login/>} />
+              <Route exact path="/register" element={<Register/>} />
+              <Route exact path="/profile" element={<Profile/>} />
+              <Route path="/user" element={<DashboardStudent/>} />
+              <Route path="/mod" element={<DashboardFaculty/>} />
+              <Route path="/admin" element={<DashboardAdmin/>} />
+            </Routes>
+          </div>
+        </div>
+        
+      </Router>
+    );
+  }
 }
-
-export default App;
+function mapStateToProps(state) {
+  const { user } = state.auth;
+  return {
+    user,
+  };
+}
+export default connect(mapStateToProps)(App);
